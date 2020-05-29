477da666dc87e2832c90a722359ebcf7e9fce540
anthony_dugouchet <adugouchet@lafourchette.com>
2020-05-29 10:35:59 +0200

    refacto code

--- js-mocha/src/gilded_rose.js
+++ js-mocha/src/gilded_rose.js
@@ -12,55 +12,72 @@ class Shop {
   }
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
-      }
+
+      let {sellIn, name, quality} = this.items[i];
+
+      let {newQuality, newSellIn } = this.updateQualityItem({
+        sellIn,
+        name,
+        quality
+      });
+
+      this.items[i].quality = newQuality;
+      this.items[i].sellIn = newSellIn;
     }
 
     return this.items;
   }
+
+  updateQualityItem({
+    sellIn,
+    name,
+    quality
+  }) {
+    let newQuality;
+
+    switch (name) {
+      case 'Aged Brie':
+        newQuality = this.increaseQuality({quality});
+        break;
+
+      case 'Conjured':
+        newQuality = quality <=0 ? 0 : quality - 2;
+        break;
+
+      case 'Sulfuras, Hand of Ragnaros':
+        newQuality = quality;
+        break;
+
+      case 'Backstage passes to a TAFKAL80ETC concert':
+        switch (true) {
+          case ( sellIn <= 10): newQuality = this.increaseQuality({quality, add:2}); break;
+          case ( sellIn <= 5): newQuality = this.increaseQuality({quality, add:3}); break;
+          case ( sellIn <= 0): newQuality = 0; break;
+          default : newQuality = this.increaseQuality({quality})
+        }
+        break;
+
+      default:
+        newQuality = this.decreaseQuality({quality, sellIn})
+    }
+
+    return {
+      newSellIn:--sellIn,
+      newQuality
+    }
+  }
+
+  decreaseQuality ({quality, sellIn}={}) {
+    return quality === 0 ? 0 : sellIn < 0 ? quality - 2 : quality - 1;
+
+  }
+
+  increaseQuality ({quality, add = 1} ={}) {
+   return quality >=50 ? 50 : quality + add
+  }
 }
+
 module.exports = {
   Item,
   Shop
-}
+};
